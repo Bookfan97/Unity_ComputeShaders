17ddda3469425220c8784d8b6b2d5fe912837c12
Noah Day <sheltielover432@gmail.com>
2021-03-31 00:15:41 -0400

    Circles Shader

--- ComputeShaders/Assets/Scripts/BufferJoy.cs
+++ ComputeShaders/Assets/Scripts/BufferJoy.cs
@@ -13,11 +13,19 @@ public class BufferJoy : MonoBehaviour
     int circlesHandle;
     int clearHandle;
 
+    struct Circle
+    {
+        public Vector2 origin;
+        public Vector2 velocity;
+        public float radius;
+    }
     public Color clearColor = new Color();
     public Color circleColor = new Color();
 
     int count = 10;
-
+    private Circle[] circleData;
+    private ComputeBuffer _buffer;
+    
     // Use this for initialization
     void Start()
     {
@@ -36,6 +44,26 @@ public class BufferJoy : MonoBehaviour
     private void InitData()
     {
         circlesHandle = shader.FindKernel("Circles");
+        uint threadGroupSizeX;
+        shader.GetKernelThreadGroupSizes(circlesHandle, out threadGroupSizeX, out _, out _);
+        int total = (int) threadGroupSizeX * count;
+        circleData = new Circle[total];
+        float speed = 100;
+        float halfSpeed = speed * 0.5f;
+        float minRadius = 10.0f;
+        float maxRadius = 30.0f;
+        float radiusRange = maxRadius - minRadius;
+
+        for (int i = 0; i < total; i++)
+        {
+            Circle circle = circleData[i];
+            circle.origin.x = Random.value * texResolution;
+            circle.origin.y = Random.value * texResolution;
+            circle.velocity.x = (Random.value * speed) - halfSpeed;
+            circle.velocity.y = (Random.value * speed) - halfSpeed;
+            circle.radius = Random.value * radiusRange + minRadius;
+            circleData[i] = circle;
+        }
     }
 
     private void InitShader()
@@ -49,6 +77,11 @@ public class BufferJoy : MonoBehaviour
 		shader.SetTexture( clearHandle, "Result", outputTexture );
         shader.SetTexture( circlesHandle, "Result", outputTexture );
 
+        int stride = (2 + 2 + 1) * sizeof(float);
+        _buffer = new ComputeBuffer(circleData.Length, stride);
+        _buffer.SetData(circleData);
+        shader.SetBuffer(circlesHandle, "circlesBuffer", _buffer);
+        
         rend.material.SetTexture("_MainTex", outputTexture);
     }
  
