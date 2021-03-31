cb1de136e7d7515107e76c4205627b94b7ec9749
Noah Day <sheltielover432@gmail.com>
2021-03-31 00:33:42 -0400

    Orbiting Stars

--- ComputeShaders/Assets/Scripts/OrbitingStars.cs
+++ ComputeShaders/Assets/Scripts/OrbitingStars.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,13 +15,17 @@ public class OrbitingStars : MonoBehaviour
     int groupSizeX;
     
     Transform[] stars;
+    ComputeBuffer resultBuffer;
+    Vector3[] output;
     
     void Start()
     {
         kernelHandle = shader.FindKernel("OrbitingStars");
         shader.GetKernelThreadGroupSizes(kernelHandle, out threadGroupSizeX, out _, out _);
         groupSizeX = (int)((starCount + threadGroupSizeX - 1) / threadGroupSizeX);
-
+        resultBuffer = new ComputeBuffer(starCount, sizeof(float)*3);
+        shader.SetBuffer(kernelHandle, "Result", resultBuffer);
+        output = new Vector3[starCount];
         stars = new Transform[starCount];
         for (int i = 0; i < starCount; i++)
         {
@@ -30,6 +35,17 @@ public class OrbitingStars : MonoBehaviour
 
     void Update()
     {
-        
+        shader.SetFloat("time", Time.time);
+        shader.Dispatch(kernelHandle, groupSizeX, 1,1);
+        resultBuffer.GetData(output);
+        for (int i = 0; i < stars.Length; i++)
+        {
+            stars[i].localPosition = output[i];
+        }
+    }
+
+    private void OnDestroy()
+    {
+        resultBuffer.Dispose();
     }
 }
